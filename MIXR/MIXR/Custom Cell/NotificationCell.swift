6100c9aac2bccc4c6de34a976152b3e65901532e
Nilesh-Patel <nileshpatel2005@gmail.com>
2015-11-25 20:31:04 +0530

    Added a table view controller for notification screen. Added a tableview controller for promotion details page.

---
+++ MIXR/MIXR/Custom Cell/NotificationCell.swift
@@ -0,0 +1,18 @@
+//
+//  NotificationCell.swift
+//  MIXR
+//
+//  Created by Nilesh Patel on 22/11/15.
+//  Copyright © 2015 MIXR LLC. All rights reserved.
+//
+
+import UIKit
+
+
+class NotificationCell: UITableViewCell {
+    @IBOutlet weak var notificationText: UILabel!
+    @IBOutlet weak var notificationTimeStamp: UILabel!
+    @IBOutlet weak var userPic: UIImageView!
+    @IBOutlet weak var cellBGView: UIView!
+}
+
