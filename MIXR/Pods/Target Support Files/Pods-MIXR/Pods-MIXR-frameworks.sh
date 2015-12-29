0843e480d6bd59f4570779b0e79ca4b15affa10b
Brendan Winter <bwinter@waypointco.com>
2015-12-28 21:24:10 -0500

    SwiftyJSON installed via CocoaPod

--- MIXR/Pods/Target Support Files/Pods-MIXR/Pods-MIXR-frameworks.sh
+++ MIXR/Pods/Target Support Files/Pods-MIXR/Pods-MIXR-frameworks.sh
@@ -86,8 +86,10 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "Pods-MIXR/Alamofire.framework"
   install_framework "Pods-MIXR/IQKeyboardManagerSwift.framework"
+  install_framework "Pods-MIXR/SwiftyJSON.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "Pods-MIXR/Alamofire.framework"
   install_framework "Pods-MIXR/IQKeyboardManagerSwift.framework"
+  install_framework "Pods-MIXR/SwiftyJSON.framework"
 fi
