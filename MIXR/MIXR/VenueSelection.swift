57f4e5b59ed395ddf85fc5d4ff98f78650efae69
Nilesh-Patel <nileshpatel2005@gmail.com>
2016-03-13 16:44:22 +0530

    Implemented the code to display the image on preview screen. Implemented the code to extract image from video. Implemented the code to play video on the preview screen when user tap on the image.

--- MIXR/MIXR/VenueSelection.swift
+++ MIXR/MIXR/VenueSelection.swift
@@ -9,15 +9,114 @@
 import Foundation
 import UIKit
 import BTNavigationDropdownMenu
+import AVFoundation
+import MediaPlayer
 
-class VenueSelection : UIViewController {
+class VenueSelection : UIViewController,UIGestureRecognizerDelegate {
     @IBOutlet weak var capturedImage: UIImageView!
+    var isVideo: Bool!
+    var capturedImageFile: UIImage!
+    var moviePlayer : MPMoviePlayerController?
+
     
     var menuView: BTNavigationDropdownMenu!
+    
+    
+    
+    func videoHasFinishedPlaying(notification: NSNotification){
+        
+        print("Video finished playing")
+        
+        /* Find out what the reason was for the player to stop */
+        let reason =
+        notification.userInfo![MPMoviePlayerPlaybackDidFinishReasonUserInfoKey]
+            as! NSNumber?
+        
+        if let theReason = reason{
+            
+            let reasonValue = MPMovieFinishReason(rawValue: theReason.integerValue)
+            
+            switch reasonValue!{
+            case .PlaybackEnded:
+                /* The movie ended normally */
+                print("Playback Ended")
+            case .PlaybackError:
+                /* An error happened and the movie ended */
+                print("Error happened")
+            case .UserExited:
+                /* The user exited the player */
+                print("User exited")
+            }
+            
+            print("Finish Reason = \(theReason)")
+            stopPlayingVideo()
+        }
+        
+    }
+    
+    func stopPlayingVideo() {
+        
+        if let player = moviePlayer{
+            NSNotificationCenter.defaultCenter().removeObserver(self)
+            player.stop()
+            player.view.removeFromSuperview()
+        }
+        
+    }
+    
+    func startPlayingVideo(){
+        
+        /* First let's construct the URL of the file in our application bundle
+        that needs to get played by the movie player */
+        
+        let paths = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)
+        let documentsDirectory: AnyObject = paths[0]
+        let dataPath = documentsDirectory.stringByAppendingPathComponent("/vid1.mp4")
+        let url = NSURL.fileURLWithPath(dataPath)
+        
+        /* If we have already created a movie player before,
+        let's try to stop it */
+        if let _ = moviePlayer{
+            stopPlayingVideo()
+        }
+        
+        /* Now create a new movie player using the URL */
+        moviePlayer = MPMoviePlayerController(contentURL: url)
+        
+        if let player = moviePlayer{
+            
+            /* Listen for the notification that the movie player sends us
+            whenever it finishes playing */
+            NSNotificationCenter.defaultCenter().addObserver(self,
+                selector: "videoHasFinishedPlaying:",
+                name: MPMoviePlayerPlaybackDidFinishNotification,
+                object: nil)
+            
+            print("Successfully instantiated the movie player")
+            
+            /* Scale the movie player to fit the aspect ratio */
+            player.scalingMode = .AspectFit
+            
+            view.addSubview(player.view)
+            
+            player.setFullscreen(true, animated: false)
+            
+            /* Let's start playing the video in full screen mode */
+            player.play()
+            
+        } else {
+            print("Failed to instantiate the movie player")
+        }
+        
+    }
+    
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+        self.navigationItem.backBarButtonItem = UIBarButtonItem(title:"", style:.Plain, target:nil, action:nil)
+
         let items = ["Most Popular", "Latest", "Trending", "Nearest", "Top Picks"]
 //        self.selectedCellLabel.text = items.first
 //        self.navigationController?.navigationBar.translucent = false
@@ -38,13 +137,52 @@ class VenueSelection : UIViewController {
         menuView.didSelectItemAtIndexHandler = {(indexPath: Int) -> () in
             print("Did select item at index: \(indexPath)")
             self.title = items[indexPath]
-//            self.selectedCellLabel.text = items[indexPath]
         }
-        
         self.navigationItem.titleView = menuView
+        
+        self.addtapGesture()
 
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func addtapGesture(){
+        let tap = UITapGestureRecognizer(target: self, action: Selector("handleTapEvent:"))
+        // we use our delegate
+        tap.delegate = self
+        // allow for user interaction
+        self.capturedImage.userInteractionEnabled = true
+        // add tap as a gestureRecognizer to tapView
+        self.capturedImage.addGestureRecognizer(tap)
+    }
+    
+    func handleTapEvent(sender: UITapGestureRecognizer? = nil) {
+        // just creating an alert to prove our tap worked!
+        if self.isVideo == true {
+            self.startPlayingVideo()
+        }
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        self.navigationController?.navigationBarHidden = false
+        super.viewWillAppear(true)
+        if self.isVideo == true {
+            let paths = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)
+            let documentsDirectory: AnyObject = paths[0]
+            let dataPath = documentsDirectory.stringByAppendingPathComponent("/vid1.mp4")
+            do {
+                let asset = AVURLAsset(URL: NSURL(fileURLWithPath: dataPath), options: nil)
+                let imgGenerator = AVAssetImageGenerator(asset: asset)
+                let cgImage = try imgGenerator.copyCGImageAtTime(CMTimeMake(0, 1), actualTime: nil)
+                let uiImage = UIImage(CGImage: cgImage)
+                capturedImage.image = uiImage
+            } catch let error as NSError {
+                print("Error generating thumbnail: \(error)")
+            }
+        }else{
+            capturedImage.image = self.capturedImageFile
+        }
+    }
+    
     
     @IBAction func postButtonTapped(sender: AnyObject){
     }
