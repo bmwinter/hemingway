5cf215444fb69ee964911df9bd3ad64a0da75e08
Michael Ciesielka <maciesielka@gmail.com>
2016-07-29 18:28:09 -0400

    [issue #7] updated the asset files and configuration of MapBox API to theme green markers

--- MIXR/MIXR/Controllers/MapViewController.swift
+++ MIXR/MIXR/Controllers/MapViewController.swift
@@ -12,14 +12,15 @@ import SwiftyJSON
 import Mapbox
 import MapKit
 
-class MapViewController: BaseViewController, MGLMapViewDelegate {
+class MapViewController: BaseViewController {
     
     @IBOutlet var mapView: MGLMapView!
     
     override func viewDidLoad() {
-        super.viewDidLoad()
+      super.viewDidLoad()
 
-        loadMapData()
+      mapView.delegate = self
+      loadMapData()
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -65,7 +66,6 @@ class MapViewController: BaseViewController, MGLMapViewDelegate {
                     point.coordinate = coordinate
                     point.title = name
                     point.subtitle = address
-                    
                     self.mapView.addAnnotation(point)
                   }
                   
@@ -74,40 +74,58 @@ class MapViewController: BaseViewController, MGLMapViewDelegate {
                 print("Response String:")
         }
     }
-    
-    func mapView(mapView: MGLMapView, rightCalloutAccessoryViewForAnnotation annotation: MGLAnnotation) -> UIView? {
-        
-        let button   = UIButton(type: UIButtonType.System) as UIButton
-        button.frame = CGRectMake(100, 100, 60, 50)
-        button.backgroundColor = UIColor.whiteColor()
-        button.setTitle("View", forState: UIControlState.Normal)
-        button.titleLabel?.textColor = UIColor.redColor()
-        button.targetForAction(Selector("buttonAction"), withSender:self )
-        
-        return button
- 
-    }
+}
+
+// MARK: MGLMapViewDelegate Protocol
+extension MapViewController: MGLMapViewDelegate {
   
-    func mapView(mapView: MGLMapView, annotation: MGLAnnotation, calloutAccessoryControlTapped control: UIControl) {
-        // hide the callout view
-        mapView.deselectAnnotation(annotation, animated: false)
-        
-        let title = annotation.title!!
-        
-        //PUSH TO VENUE PROFILE use TITLE AS ID
-        
-        
-        //potentially dangerous if multiple bars named identically
-        //saves from inheriting MGLAnnotation, adding a venue_id string, extending the MGLMapViewDelegate with passing the VenueAnnotation (custom class with string) to this function annotation: VenueAnnotation. problem is that Mapbox is in Objective C so extending the protocol requires bridging header but the Swift VenueAnnotation object won't import in the extened protocol class nor could I import the extended protocol class
-        
-        UIAlertView(title: "\(annotation.title!!)", message: "A lovely \(title) bar!", delegate: nil, cancelButtonTitle: nil, otherButtonTitles: "OK").show()
+  func mapView(mapView: MGLMapView, rightCalloutAccessoryViewForAnnotation annotation: MGLAnnotation) -> UIView? {
+    
+    let button   = UIButton(type: UIButtonType.System) as UIButton
+    button.frame = CGRectMake(100, 100, 60, 50)
+    button.backgroundColor = UIColor.whiteColor()
+    button.setTitle("View", forState: UIControlState.Normal)
+    button.titleLabel?.textColor = UIColor.redColor()
+    button.targetForAction(Selector("buttonAction"), withSender:self )
+    
+    return button
+    
+  }
+  
+  func mapView(mapView: MGLMapView, imageForAnnotation annotation: MGLAnnotation) -> MGLAnnotationImage? {
+    
+    let filename = "locationDroplet"
+    var annotationImage = mapView.dequeueReusableAnnotationImageWithIdentifier(filename)
+    
+    if annotationImage == nil {
+      var image = UIImage(named: filename)!
+      
+      // disregard the image padding added to compensate for no anchor point offset in API
+      image = image.imageWithAlignmentRectInsets(UIEdgeInsetsMake(0, 0, image.size.height/2, 0))
+      annotationImage = MGLAnnotationImage(image: image, reuseIdentifier: filename)
     }
     
-    func mapView(mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool {
-        return true
-    }
+    return annotationImage
+  }
+  
+  func mapView(mapView: MGLMapView, annotation: MGLAnnotation, calloutAccessoryControlTapped control: UIControl) {
+    // hide the callout view
+    mapView.deselectAnnotation(annotation, animated: false)
+    
+    let title = annotation.title!!
+    
+    //PUSH TO VENUE PROFILE use TITLE AS ID
     
     
+    //potentially dangerous if multiple bars named identically
+    //saves from inheriting MGLAnnotation, adding a venue_id string, extending the MGLMapViewDelegate with passing the VenueAnnotation (custom class with string) to this function annotation: VenueAnnotation. problem is that Mapbox is in Objective C so extending the protocol requires bridging header but the Swift VenueAnnotation object won't import in the extened protocol class nor could I import the extended protocol class
+    
+    UIAlertView(title: "\(annotation.title!!)", message: "A lovely \(title) bar!", delegate: nil, cancelButtonTitle: nil, otherButtonTitles: "OK").show()
+  }
+  
+  func mapView(mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool {
+    return true
+  }
 }
 
 
